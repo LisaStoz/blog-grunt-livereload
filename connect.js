7b284a896419057a1f470277d4e28f8505f78f96
Jelizaveta Potapova <lisastozdev@gmail.com>
2015-03-22 14:52:02 +0000

    connect configuration

--- connect.js
+++ connect.js
@@ -1,10 +1,10 @@
 connect: {
   dev: {
     options: {
-      port: 8083,
-      base: 'app',
-      keepalive: true,
-      hostname: '127.0.0.1'
+      port: 8083, // port to run your static file server on
+      base: 'app', // directory where you want to server static files
+      keepalive: true, // to prevent server from stopping once all files were compiled
+      hostname: '127.0.0.1' // hostname to access your static files
     }
   }
 }
