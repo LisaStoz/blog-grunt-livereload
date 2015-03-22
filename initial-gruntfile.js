4b6adf1cda65750cc08bbd5f19fa5779c90e67b7
Jelizaveta Potapova <lisastozdev@gmail.com>
2015-03-22 14:43:54 +0000

    initial grunt file

---
+++ initial-gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.initConfig({
+      watch: {
+        // ...
+      },
+      connect: {
+        // ...
+      },
+      concurrent: {
+        // ...
+      }
+
+  });
+
+};
